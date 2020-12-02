eb7f60cf83e8d507a95a5167c8f4abe9d20416e4
magnus <StoneSwine@users.noreply.github.com>
2020-12-02 19:17:05 +0100

    Removed middleware for speeeeeed

--- web/settings.py
+++ web/settings.py
@@ -44,9 +44,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'htmlmin.middleware.HtmlMinifyMiddleware',
-    'htmlmin.middleware.MarkRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'web.urls'
@@ -123,5 +120,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-HTML_MINIFY = True
